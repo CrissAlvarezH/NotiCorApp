2cac3fd2adfaf0c98ba2d1a26f4cd1cd839459c7
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2019-01-26 09:12:08 -0500

    CreÃ³ menÃº inferior

--- app/src/main/java/helpers/cristian/com/ubiety/MainActivity.java
+++ app/src/main/java/helpers/cristian/com/ubiety/MainActivity.java
@@ -3,42 +3,65 @@ package helpers.cristian.com.ubiety;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private ImageView imgItemMapa, imgItemBloques, imgItemNoti;
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        imgItemMapa = findViewById(R.id.bottom_item_map);
+        imgItemBloques = findViewById(R.id.bottom_item_bloques);
+        imgItemNoti = findViewById(R.id.bottom_item_noti);
+        pager = findViewById(R.id.pager_pages);
+
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(pagerAdapter);
     }
 
+    private class PagerAdapter extends FragmentPagerAdapter {
+        private ArrayList<Fragment> fragmentos = new ArrayList<>();
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fragmentos.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentos.size();
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            Fragment fragment = (Fragment) super.instantiateItem(container, position);
+
+            fragmentos.set(position, fragment);// Actualizamos la instancia del afragment
+
+            return fragment;
+        }
+    }
 }
