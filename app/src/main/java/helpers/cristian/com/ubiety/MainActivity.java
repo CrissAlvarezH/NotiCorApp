45853ba5f19993b4bdcdf9f4aa4517ec82c29908
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2019-01-26 10:09:25 -0500

    CreÃ fragments, diÃ³ funcionalidad l menÃºinferior

--- app/src/main/java/helpers/cristian/com/ubiety/MainActivity.java
+++ app/src/main/java/helpers/cristian/com/ubiety/MainActivity.java
@@ -16,7 +16,16 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+import helpers.cristian.com.ubiety.fragmentos.BloquesFragment;
+import helpers.cristian.com.ubiety.fragmentos.MapaFragment;
+import helpers.cristian.com.ubiety.fragmentos.NotificacionesFragment;
+import helpers.cristian.com.ubiety.glide.GlideApp;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private final int POS_MAPA = 0;
+    private final int POS_BLOQUES = 1;
+    private final int POS_NOTI = 2;
 
     private ImageView imgItemMapa, imgItemBloques, imgItemNoti;
     private ViewPager pager;
@@ -33,12 +42,40 @@ public class MainActivity extends AppCompatActivity {
         imgItemNoti = findViewById(R.id.bottom_item_noti);
         pager = findViewById(R.id.pager_pages);
 
+        imgItemBloques.setOnClickListener(this);
+        imgItemMapa.setOnClickListener(this);
+        imgItemNoti.setOnClickListener(this);
+
+        // Por defecto el mapa está seleccionado
+        GlideApp.with(this)
+                .load("")
+                .placeholder(R.drawable.google_maps_colores)
+                .into(imgItemMapa);
+
+        GlideApp.with(this)
+                .load("")
+                .placeholder(R.drawable.edificios_outline)
+                .into(imgItemBloques);
+
+        GlideApp.with(this)
+                .load("")
+                .placeholder(R.drawable.noti_outline)
+                .into(imgItemNoti);
+
+
         pagerAdapter = new PagerAdapter(getSupportFragmentManager());
         pager.setAdapter(pagerAdapter);
+
+        pager.addOnPageChangeListener(listenerCambioDePagina);
     }
 
     private class PagerAdapter extends FragmentPagerAdapter {
-        private ArrayList<Fragment> fragmentos = new ArrayList<>();
+
+        private ArrayList<Fragment> fragmentos = new ArrayList<Fragment>() {{
+            add(POS_MAPA, new MapaFragment());
+            add(POS_BLOQUES, new BloquesFragment());
+            add(POS_NOTI, new NotificacionesFragment());
+        }};
 
         public PagerAdapter(FragmentManager fm) {
             super(fm);
@@ -64,4 +101,110 @@ public class MainActivity extends AppCompatActivity {
             return fragment;
         }
     }
+
+    private ViewPager.OnPageChangeListener listenerCambioDePagina = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {}
+
+        @Override
+        public void onPageSelected(int pos) {
+            switch (pos) {
+                case POS_MAPA:
+
+                    GlideApp.with( MainActivity.this )
+                            .load("")
+                            .placeholder(R.drawable.google_maps_colores)
+                            .into(imgItemMapa);
+
+                    GlideApp.with(MainActivity.this)
+                            .load("")
+                            .placeholder(R.drawable.edificios_outline)
+                            .into(imgItemBloques);
+
+                    GlideApp.with(MainActivity.this)
+                            .load("")
+                            .placeholder(R.drawable.noti_outline)
+                            .into(imgItemNoti);
+
+                    break;
+                case POS_BLOQUES:
+
+                    GlideApp.with(MainActivity.this)
+                            .load("")
+                            .placeholder(R.drawable.google_maps_outline)
+                            .into(imgItemMapa);
+
+
+                    GlideApp.with( MainActivity.this )
+                            .load("")
+                            .placeholder(R.drawable.edificios_colores)
+                            .into(imgItemBloques);
+
+                    GlideApp.with(MainActivity.this)
+                            .load("")
+                            .placeholder(R.drawable.noti_outline)
+                            .into(imgItemNoti);
+
+                    break;
+                case POS_NOTI:
+
+                    GlideApp.with(MainActivity.this)
+                            .load("")
+                            .placeholder(R.drawable.google_maps_outline)
+                            .into(imgItemMapa);
+
+                    GlideApp.with(MainActivity.this)
+                            .load("")
+                            .placeholder(R.drawable.edificios_outline)
+                            .into(imgItemBloques);
+
+                    GlideApp.with( MainActivity.this )
+                            .load("")
+                            .placeholder(R.drawable.noti_colores)
+                            .into(imgItemNoti);
+
+                    break;
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int pos) {}
+    };
+
+    @Override
+    public void onClick(View view) {
+        switch ( view.getId() ) {
+            case R.id.bottom_item_map:
+                pager.setCurrentItem(POS_MAPA);
+
+                break;
+            case R.id.bottom_item_bloques:
+                pager.setCurrentItem(POS_BLOQUES);
+
+                break;
+            case R.id.bottom_item_noti:
+                pager.setCurrentItem(POS_NOTI);
+
+                break;
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/helpers/cristian/com/ubiety/fragmentos/BloquesFragment.java
@@ -0,0 +1,27 @@
+package helpers.cristian.com.ubiety.fragmentos;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import helpers.cristian.com.ubiety.R;
+
+public class BloquesFragment extends Fragment {
+
+    public BloquesFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View vista = inflater.inflate(R.layout.fragment_bloques, container, false);
+
+
+
+        return vista;
+    }
+
+}
